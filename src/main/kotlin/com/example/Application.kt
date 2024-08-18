12bd28f60c9e4a5f69992c9392d0c79d9814face
kroune <123553746+kroune@users.noreply.github.com>
2024-08-18 17:19:57 +0300

    pod to pod communication

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -36,8 +36,10 @@ fun main() {
 }
 
 fun Application.module() {
+    val ip = "127.0.0.1:5432"
+    val dns = "postgres-service.default.svc.cluster.local"
     Database.connect(
-        "jdbc:postgresql://127.0.0.1:5432/postgres",
+        "jdbc:postgresql://$dns/postgres",
         driver = "org.postgresql.Driver",
         user = "postgres",
         password = "1234"
