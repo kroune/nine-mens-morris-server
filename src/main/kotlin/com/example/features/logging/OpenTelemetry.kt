ed73e1d372cea19e1be1a719b64a6dc17a2b9272
kroune <123553746+kroune@users.noreply.github.com>
2024-10-22 00:29:52 +0300

    important fix

--- src/main/kotlin/com/example/features/logging/OpenTelemetry.kt
+++ src/main/kotlin/com/example/features/logging/OpenTelemetry.kt
@@ -3,7 +3,7 @@ package com.example.features.logging
 val openTelemetryEndpoint: String = run {
     val isInK8s = System.getenv("IS_IN_K8S") == "1"
     val localhost = "http://127.0.0.1:4317"
-    val podDomain = "http://my-release-signoz-otel-collector.default.svc.cluster.local"
+    val podDomain = "http://my-release-signoz-otel-collector.platform.svc.cluster.local"
     val endpoint = if (isInK8s) podDomain else localhost
     endpoint
 }
