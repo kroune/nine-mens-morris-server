b53d33d2ca62b4d45d40ac503be0d988ece38e5d
kroune <123553746+kroune@users.noreply.github.com>
2025-04-04 18:44:54 +0300

    allow CORS

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -129,8 +129,8 @@ fun Application.applyPlugins(includeRateLimitPlugin: Boolean = true) {
     install(CORS) {
         allowMethod(HttpMethod.Get)
         allowMethod(HttpMethod.Post)
-        allowSameOrigin = true
         allowHost("github.io", listOf("http", "https"), listOf("kroune"))
+        allowHost("nine-men-s-morris.me", listOf("http", "https"), listOf("play"))
     }
     install(Authentication) {
         basic("prometheus") {
