27f3c61a7064a2654d7c80928d25879f3b5e6140
kroune <123553746+kroune@users.noreply.github.com>
2025-04-13 22:45:52 +0300

    CORS moment

--- src/main/kotlin/io/github/kroune/Application.kt
+++ src/main/kotlin/io/github/kroune/Application.kt
@@ -131,7 +131,7 @@ fun Application.applyPlugins(includeRateLimitPlugin: Boolean = true) {
         allowMethod(HttpMethod.Post)
         allowHeader(HttpHeaders.ContentType)
         allowHost("github.io", listOf("http", "https"), listOf("kroune"))
-        allowHost("nine-men-s-morris.me", listOf("http", "https"), listOf("play"))
+        allowHost("nine-men-s-morris.me", listOf("http", "https"), listOf("play", "dev"))
     }
     install(Authentication) {
         basic("prometheus") {
